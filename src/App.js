eae44cee44d08ce9c64b09ab83b842091a62f8a6
saniyamemon2702 <saniyamemon7862@gmail.com>
2023-01-14 17:53:54 -0500

    Created: Project and Tailwind CSS added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-center text-3xl font-bold underline">
+      Hello SANeesh!
+    </h1>
+  );
+}
+
+export default App;
